5a4850266b91565d0a6a5149d0b33c29bce04b5b
Jennifer L Brown <jlynn81@fullsail.edu>
2014-03-12 20:35:38 -0500

    Completed Weekly Self Audit [Delivers #66830626]
    Commented all code to reflect proper usage of AngularJS and Twitter
    Bootstrap.  Fine Details are still being work regarding colors and
    layout.

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,3 +1,4 @@
+//Information for pulling data from the API to be utilized for the project
 app.controller("mainController", function($scope, $http){
     $scope.apiKey = "43232be0b3972a27cbd7cf7208225b9f";
     $scope.results = [];
@@ -9,23 +10,31 @@ app.controller("mainController", function($scope, $http){
     $scope.orderField = "Air Date"; //Default order field
     $scope.orderReverse = false;
     $scope.init = function() {
+
         //API requires a start date
         var today = new Date();
+
         //Create the date string and ensure leading zeros if required
         var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
         $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
+
             //As we are getting our data from an external source, we need to format the data so we can use it to our desired affect
             //For each day get all the episodes
             angular.forEach(data, function(value, index){
+
                 //The API stores the full date separately from each episode. Save it so we can use it later
                 var date = value.date;
+
                 //For each episodes add it to the results array
                 angular.forEach(value.episodes, function(tvshow, index){
+
                     //Create a date string from the timestamp so we can filter on it based on user text input
                     tvshow.date = date; //Attach the full date to each episode
                     $scope.results.push(tvshow);
+
                     //Loop through each genre for this episode
                     angular.forEach(tvshow.show.genres, function(genre, index){
+
                         //Only add to the availableGenres array if it doesn't already exist
                         var exists = false;
                         angular.forEach($scope.availableGenres, function(avGenre, index){
@@ -46,7 +55,7 @@ app.controller("mainController", function($scope, $http){
     };
     $scope.setGenreFilter = function(genre) {
         $scope.genreFilter = genre;
-    }
+    };
     $scope.customOrder = function(tvshow) {
         switch ($scope.orderField) {
             case "Air Date":
