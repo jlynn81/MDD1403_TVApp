bdace4f1dea3516256d84d6b2319a98aa3de2704
Jennifer L Brown <jlynn81@fullsail.edu>
2014-03-27 10:53:40 -0500

    Initial Commit

---
+++ TVApp/TVApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TVApp
+//
+//  Created by Jennifer Brown on 3/27/14.
+//  Copyright (c) 2014 WDD1403. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TVApp/TVAppTests/TVAppTests.m
@@ -0,0 +1,34 @@
+//
+//  TVAppTests.m
+//  TVAppTests
+//
+//  Created by Jennifer Brown on 3/27/14.
+//  Copyright (c) 2014 WDD1403. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TVAppTests : XCTestCase
+
+@end
+
+@implementation TVAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
